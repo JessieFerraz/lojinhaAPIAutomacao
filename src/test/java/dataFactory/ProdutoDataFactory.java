819c4b311b219b777811f54a897323de14ab4b6c
jessica <jessieferraz@gmail.com>
2023-11-05 20:34:30 +0000

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/dataFactory/ProdutoDataFactory.java
@@ -0,0 +1,39 @@
+package dataFactory;
+
+import pojo.ComponentePojo;
+import pojo.ProdutoPojo;
+import pojo.UsuarioPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDataFactory {
+    public static ProdutoPojo criarProdutoComumComOValorIgualA(double valor){
+        ProdutoPojo produto = new ProdutoPojo();
+        produto.setProdutoNome("PlayStation 5");
+        produto.setProdutoValor(valor);
+
+        List<String> cores = new ArrayList<>();
+        cores.add("preto");
+        cores.add("branco");
+        produto.setProdutoCores(cores);
+
+        produto.setProdutoUrlMock("");
+
+        List<ComponentePojo> componentes = new ArrayList<>();
+
+        ComponentePojo componente = new ComponentePojo();
+        componente.setComponenteNome("Controle");
+        componente.setComponenteQuantidade(1);
+        componentes.add(componente);
+
+        ComponentePojo segundoComponente = new ComponentePojo();
+        segundoComponente.setComponenteNome("Cabo");
+        segundoComponente.setComponenteQuantidade(1);
+        componentes.add(segundoComponente);
+
+        produto.setComponentes(componentes);
+
+        return produto;
+    }
+}
